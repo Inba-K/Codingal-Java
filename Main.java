0213ac6d74cbe86cf6708f65768fb75951717a92
Inba-K <inba.karthikeyan18@gmail.com>
2024-08-26 17:14:34 -0700

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,8 @@
+public class Addition{
+    public static void main(String args[]) {
+        int a=3;
+        int b=2;
+        int sum=a+b;
+        System.out.println("The sum of 3 and 2 is "+sum);
+    }
+}
