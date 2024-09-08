6a8fa82e43d17f9289c9ed1b739be97bdd7c6583
Inba-K <inba.karthikeyan18@gmail.com>
2024-09-08 14:52:56 -0700

    Talks about me

---
+++ Name.java
@@ -0,0 +1,6 @@
+public class Name{
+    public static void main(String args[]){
+        System.out.println("My name is Inba Karthikeyan, and I like to build legos.");
+        System.out.println("My name also means happy!");
+    }
+}
