d5b01da39af16f1095046d86e43c31c553d6bf3e
Inba-K <inba.karthikeyan18@gmail.com>
2024-09-08 14:48:20 -0700

    Git commits for ArithmeticOperations

---
+++ ArithmeticOperators.java
@@ -0,0 +1,11 @@
+public class ArithmeticOperators{
+    public static void main(String args[]){
+        System.out.println("Hello World!");
+        int a=10;
+        int b=20;
+        System.out.println(a*b);
+        System.out.println(b/a);
+        System.out.println(a+b);
+        System.out.println(b-a);
+    }
+}
