a68610c0e23178e0854ae6103e2cb5c335b1a2ba
Inba-K <inba.karthikeyan18@gmail.com>
2024-09-08 17:39:20 -0700

    Project done

---
+++ Cloth.java
@@ -0,0 +1,18 @@
+public class Cloth {
+    public static void main(String args[]){
+        boolean sunny=true;
+        boolean cloudy=false;
+        if (cloudy==false){
+            if (sunny==true){
+                System.out.println("Make sure to use sunscreen.");
+            }else{
+                System.out.println("Today is going to be a fair-weathered day");
+            }
+
+        }else{
+            System.out.println("Today is going to be cloudy or rainy.");
+
+        }
+
+        }
+    }
