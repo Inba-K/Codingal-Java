3862a0bc48ea9055f84c6bd92c692a2b02ce6aad
Inba-K <inba.karthikeyan18@gmail.com>
2024-09-22 17:42:52 -0700

    Loops project done

---
+++ Loops.java
@@ -0,0 +1,11 @@
+public class Loops {
+    public static void main(String args[]){
+        int i=0;
+        while (i<=10){
+            System.out.println(i);
+            i+=1;
+        }for (i=10;i>=0;i--){
+            System.out.println(i);
+        }
+    }
+}
