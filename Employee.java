e96a02386f92bf8e966145821beb88be0758d59d
Inba-K <inba.karthikeyan18@gmail.com>
2024-10-06 17:51:00 -0700

    Employee project done

---
+++ Employee.java
@@ -0,0 +1,15 @@
+class Employee{
+    String n;
+    int age;
+    int id;
+    int wage;
+    Employee(){
+        n="Barbra";
+        age=25;
+        id=69345;
+        wage=100;
+    }
+    void display(){
+        System.out.println(n+"  |  "+ age +"  |  "+id+"  |  "+wage);
+    }
+}
