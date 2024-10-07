ca931ffca1c1cf3240ff10eceef304be1d3680dc
Inba-K <inba.karthikeyan18@gmail.com>
2024-10-06 18:07:29 -0700

    Student project done

---
+++ Student.java
@@ -0,0 +1,13 @@
+class Student{
+    String n;
+    int age;
+    int id;
+    Student(){
+        n="Bala";
+        age=8;
+        id=1709;
+    }
+    void display(){
+        System.out.println(n+" | "+age+" | "+id);
+    }
+}
