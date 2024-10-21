dbe633787e3319eec32a323130b1146fc92bd7c7
Inba-K <inba.karthikeyan18@gmail.com>
2024-10-20 19:53:46 -0700

    Overriding Overloading project done

---
+++ Method.java
@@ -0,0 +1,23 @@
+class Country{
+    void display(){
+        System.out.println("There are many countries");
+    }
+    void display(int b){
+        System.out.println("There are "+b+" countries in the world.");
+    }
+}
+class India extends Country{
+    @Override
+    void display(){
+        System.out.println("India is a country.");
+    }
+}
+public class Method{
+    public static void main(String args[]) {
+        Country obj1=new Country();
+        Country obj2=new India();
+        obj1.display();
+        obj1.display(110);
+        obj2.display();
+    }
+}
